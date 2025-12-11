386c8d1dc7e4d25df4f16f797b22d75a7bf9779a
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-11 16:51:05 +0530

    built basic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,21 @@
+import { SquarePlus } from "lucide-react";
+import DarkMode from "./Darkmode";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
-export default Navbar
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">Store</a>
+      </div>
+      <div className="navbar-end gap-3">
+        <button className="btn btn-ghost btn-circle">
+          <SquarePlus/>
+        </button>
+        <button className="btn btn-ghost btn-circle">
+          <DarkMode />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
