911a2a8a61059be8b0f58fad337b80bda1596381
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-12 12:55:35 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import Navbar from "./components/Navbar"
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Createpage from "./pages/Createpage";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<Createpage />} />
+      </Routes>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
