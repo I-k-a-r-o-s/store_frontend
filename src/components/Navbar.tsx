feb4b89be5060101a5bb8bd909fe551c25f78108
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-12 12:59:12 +0530

    added links to pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,21 @@
 import { SquarePlus } from "lucide-react";
 import DarkMode from "./Darkmode";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">Store</a>
+        <Link to={"/"}>
+          <a className="btn btn-ghost text-xl">Store</a>
+        </Link>
       </div>
       <div className="navbar-end gap-3">
-        <button className="btn btn-ghost btn-circle">
-          <SquarePlus/>
-        </button>
+        <Link to={"/create"}>
+          <button className="btn btn-ghost btn-circle">
+            <SquarePlus />
+          </button>
+        </Link>
         <button className="btn btn-ghost btn-circle">
           <DarkMode />
         </button>
