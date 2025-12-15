a9b1c615397422a034b0f4f5fd845961db915829
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-15 17:21:25 +0530

    built product card

---
+++ src/components/Productcard.tsx
@@ -0,0 +1,37 @@
+import type { Item } from "../lib/types";
+
+interface ProductCardProps {
+  item: Item;
+}
+
+const Productcard = ({ item }: ProductCardProps) => {
+  return (
+    <div className="card bg-base-100 shadow-sm w-full max-w-sm">
+      <figure>
+        <img
+          src={item.image}
+          alt={item.name}
+          className="h-48 w-full object-cover"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-xl">{item.name}</h2>
+        <h3 className="mt-2 font-semibold text-lg">{item.price}</h3>
+        <div className="text-sm text-gray-500">
+          <p>
+            Added to inventory at:{" "}
+            {item.createdAt ? new Date(item.createdAt).toLocaleString() : "N/A"}
+          </p>
+          <p>
+            Updated at:{" "}
+            {item.updatedAt ? new Date(item.updatedAt).toLocaleString() : "N/A"}
+          </p>
+        </div>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Productcard;
